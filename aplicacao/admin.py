9f38493bceb09e2f2c75d03781a02c2861bf47a3
ricardo martins <ricardosm@rede.ulbra.br>
2020-11-28 09:02:18 -0300

    add event

--- aplicacao/admin.py
+++ aplicacao/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Clientes, Profissionais, Especializacao, Servicos, Event
+from .models import (
+    Clientes, 
+    Profissionais, 
+    Especializacao, 
+    Servicos,
+    AgendaServico
+)
+from .utils import AgendaEvent
 from django.urls import reverse
 import datetime
 import calendar
 from calendar import HTMLCalendar
 from django.utils.safestring import mark_safe
+
 class ClientesAdmin(admin.ModelAdmin):
 
     fields = (
@@ -43,15 +51,15 @@ class ProfissionaisAdmin(admin.ModelAdmin):
     ]
     model = Profissionais
 
+class AgendaServicoAdmin(admin.ModelAdmin):
 
-class EventAdmin(admin.ModelAdmin):
-    list_display = ['day', 'start_time', 'end_time', 'notes']
-    model = Event
-
+    list_display = ['cliente', 'data', 'hora', 'valor', 'observacao']
+    change_list_template = 'admin/events/agenda.html'
+    # model = AgendaServico
     def changelist_view(self, request, extra_context=None):
         after_day = request.GET.get('day__gte', None)
         extra_context = extra_context or {}
-
+        
         if not after_day:
             d = datetime.date.today()
         else:
@@ -61,28 +69,22 @@ class EventAdmin(admin.ModelAdmin):
             except:
                 d = datetime.date.today()
 
-        previous_month = datetime.date(year=d.year, month=d.month, day=1)  # find first day of current month
-        previous_month = previous_month - datetime.timedelta(days=1)  # backs up a single day
-        previous_month = datetime.date(year=previous_month.year, month=previous_month.month,
-                                       day=1)  # find first day of previous month
+        previous_month = datetime.date(year=d.year, month=d.month, day=(d.day-7))  # find first day of current month
+        next_month = datetime.date(year=d.year, month=d.month, day=(d.day+7))  # find last day of current month
+        extra_context['previous_month'] = reverse('admin:events_event_changelist') + '?day__gte=' + str(
+            previous_month)
+        extra_context['next_month'] = reverse('admin:events_event_changelist') + '?day__gte=' + str(next_month)
 
-        last_day = calendar.monthrange(d.year, d.month)
-        next_month = datetime.date(year=d.year, month=d.month, day=last_day[1])  # find last day of current month
-        next_month = next_month + datetime.timedelta(days=1)  # forward a single day
-        next_month = datetime.date(year=next_month.year, month=next_month.month,
-                                   day=1)  # find first day of next month
-
-        # extra_context['previous_month'] = reverse('admin:events_event_changelist') + '?day__gte=' + str(
-        #     previous_month)
-        # extra_context['next_month'] = reverse('admin:events_event_changelist') + '?day__gte=' + str(next_month)
-
-        cal = HTMLCalendar()
+        cal = AgendaEvent()
         html_calendar = cal.formatmonth(d.year, d.month, withyear=True)
         html_calendar = html_calendar.replace('<td ', '<td  width="150" height="150"')
-        extra_context['calendar'] = mark_safe(html_calendar)
-        return super(EventAdmin, self).changelist_view(request, extra_context)
+        extra_context['agenda'] = mark_safe(html_calendar)
+        print(extra_context)
+        return super(AgendaServicoAdmin, self).changelist_view(request, extra_context)
 
 
-admin.site.register(Event, EventAdmin)
+
+
 admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Profissionais, ProfissionaisAdmin)
+admin.site.register(AgendaServico, AgendaServicoAdmin)
--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.urls import reverse
 
 
 class Clientes(models.Model):
@@ -61,41 +66,14 @@ class Servicos(models.Model):
         verbose_name = "Serviço"
         verbose_name_plural = "Serviços"
 
-
-class Event(models.Model):
-    day = models.DateField('Dias do evento', help_text='Dias do evento')
-    start_time = models.TimeField('Hora inicial', help_text='Hora inicial')
-    end_time = models.TimeField('Hora final', help_text=u'Hora final')
-    notes = models.TextField('Anotações', help_text='TAnotações', blank=True, null=True)
+class AgendaServico(models.Model):
+    cliente = models.ForeignKey(Clientes,on_delete=models.CASCADE)
+    servico = models.ManyToManyField(Servicos)
+    data = models.DateField()
+    hora = models.TimeField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    observacao = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
-        verbose_name = 'Agenda'
-        verbose_name_plural = 'Agenda'
-
-    def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
-        overlap = False
-        if new_start == fixed_end or new_end == fixed_start:  # edge case
-            overlap = False
-        elif (new_start >= fixed_start and new_start <= fixed_end) or (new_end >= fixed_start and new_end <= fixed_end):  # innner limits
-            overlap = True
-        elif new_start <= fixed_start and new_end >= fixed_end:  # outter limits
-            overlap = True
-        print(overlap)
-        return overlap
-
-    def get_absolute_url(self):
-        url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
-        return u'<a href="%s">%s</a>' % (url, str(self.start_time))
-
-    def clean(self):
-        if self.end_time <= self.start_time:
-            raise ValidationError('Hora final deve ser após a hora inicial')
-
-        events = Event.objects.filter(day=self.day)
-        if events.exists():
-            for event in events:
-                print(event.start_time, event.end_time)
-                if self.check_overlap(event.start_time, event.end_time, self.start_time, self.end_time):
-                    raise ValidationError(
-                        'Há uma sobreposição com outro evento: ' + str(event.day) + ', ' + str(
-                            event.start_time) + '-' + str(event.end_time))
+        verbose_name = "Agenda de Serviço"
+        verbose_name_plural = "Agenda de Serviços"
