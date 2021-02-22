8213ac24df45facf21796b7ed22bbe897d56a900
ricardo martins <ricardosm@rede.ulbra.br>
2021-02-22 12:14:10 -0300

    atualiza automatico ajustes

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,19 +1,25 @@
 // console.log("teste pega valor")
 function pegaValor() {
     var total = 0.0;
-    if(document.getElementById("agendaservico_form")){
-        var valor = document.getElementById("id_valor").value;
-        var form = document.getElementsByClassName("field-valor_venda");
-        for (let i=0;i < form.length -1 ;i++) {
-            var produto = document.getElementById("id_pedido_set-"+i+"-valor_venda").value;
-            total = total + parseFloat(produto);
+    function valor(total) {
+        if(document.getElementById("agendaservico_form")){
+            var valor = document.getElementById("id_valor").value;
+            total = total + parseFloat(valor);
+            var form = document.getElementsByClassName("field-valor_venda");
+            for (let i=0;i < form.length -1 ;i++) {             
+                var produto = document.getElementById("id_pedido_set-"+i+"-valor_venda").value;
+                total = total + parseFloat(produto);
+            }
+            return total;
         }
-        total = total + parseFloat(valor);
     }
+
     if(document.getElementById("pagamento_set-group")){
+
         var le = document.getElementById("pagamento_set-group");
         var href = le.children[0].children[4].children[1].children[1].children[1].children[0].children[0];
         href.addEventListener("click",function(){
+            total = total + valor(total);
             var form = document.getElementById("id_pagamento_set-0-valor");
             form.value = total;
         });
