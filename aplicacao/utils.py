be82fbd340afa00fad3f6f3568f56cf7351fd064
ricardosmbr <ricardosm@rede.ulbra.br>
2021-05-19 19:02:55 -0300

    atualização add celery

--- aplicacao/utils.py
+++ aplicacao/utils.py
@@ -3,6 +3,7 @@ from datetime import datetime as dtime, date, time
 from datetime import timedelta
 from .models import AgendaServico, Profissionais
 from configuracao.models import Configuracao, Dias_semana
+
 # from .models import Event
 
 
@@ -10,17 +11,16 @@ class AgendaEvent(HTMLCalendar):
     def __init__(self, events=None):
         super(AgendaEvent, self).__init__()
         self.events = events
-    def formatday(self, day, hora,pro, events):
+
+    def formatday(self, day, hora, pro, events):
         """
         Return a day as a table cell.
         """
         # print(dir(hora))
         # print(hora.seconds/3600)
-        if(hora):
+        if hora:
             events_from_day = events.filter(
-                data__day=day,
-                hora__hour=int(hora.seconds/3600),
-                profissional=pro
+                data__day=day, hora__hour=int(hora.seconds / 3600), profissional=pro
             )
         # print(events_from_day)
         events_html = "<ul>"
@@ -32,17 +32,17 @@ class AgendaEvent(HTMLCalendar):
         if day == 0:
             return '<td class="noday">&nbsp;</td>'  # day outside month
         else:
-            return '<td class="">%s</td>' % ( events_html)
+            return '<td class="">%s</td>' % (events_html)
 
     def formatweek(self, theweek, events):
         """
         Return a complete week as a table row.
         """
-        s = ''.join(self.formatday(d, wd, events) for (d, wd) in theweek)
+        s = "".join(self.formatday(d, wd, events) for (d, wd) in theweek)
 
-        return '<tr>%s</tr>' % s
+        return "<tr>%s</tr>" % s
 
-    def formatmonth(self,data, theyear, themonth, withyear=True):
+    def formatmonth(self, data, theyear, themonth, withyear=True):
         """
         Return a formatted month as a table.
         """
@@ -50,52 +50,85 @@ class AgendaEvent(HTMLCalendar):
         events = AgendaServico.objects.filter(data__month=themonth)
         proficionais = Profissionais.objects.all().order_by("nome")
         confi = Configuracao.objects.all().first()
-        hora  = Dias_semana.objects.filter(id_configuracao=confi)
+        hora = Dias_semana.objects.filter(id_configuracao=confi)
         today = data
-        dias = ('Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo')
-        meses = ('Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Nvembro','Dezembro')
+        dias = (
+            "Segunda-feira",
+            "Terça-feira",
+            "Quarta-feira",
+            "Quinta-feira",
+            "Sexta-feira",
+            "Sábado",
+            "Domingo",
+        )
+        meses = (
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Nvembro",
+            "Dezembro",
+        )
         v = []
         a = v.append
-        a((dias[today.weekday()] +" "+ str(today.day) +" de " + meses[today.month-1]+" de "+ str(today.year)))
+        a("<div>")
+        a(
+            (
+                dias[today.weekday()]
+                + " "
+                + str(today.day)
+                + " de "
+                + meses[today.month - 1]
+                + " de "
+                + str(today.year)
+            )
+        )
+        a("</div>")
         a('<table border="0" cellpadding="0" cellspacing="0" class="month">')
-        profi = '<tr><th>Horário</th>'
+        profi = "<tr><th>Horário</th>"
         for pro in proficionais:
-            profi = profi + '<th>' + pro.nome+'</th>'
-        profi = profi+'</tr>'
-        a('\n')
+            profi = profi + "<th>" + pro.nome + "</th>"
+        profi = profi + "</tr>"
+        a("\n")
         a(profi)
-        linha = '<tr>'
+        linha = "<tr>"
         qtde = 0
         for coluna in hora:
-            if(today.weekday()==0 and coluna.nome =='SEGUNDA'):
-                qtde = (coluna.hora_fim.hour - coluna.hora_inicio.hour)
-            elif(today.weekday()==1 and coluna.nome =='TERÇA'):
-                qtde = (coluna.hora_fim.hour - coluna.hora_inicio.hour)
-            elif(today.weekday()==2 and coluna.nome =='QUARTA'):
-                qtde = (coluna.hora_fim.hour - coluna.hora_inicio.hour)
-            elif(today.weekday()==3 and coluna.nome =='QUINTA'):
-                qtde = (coluna.hora_fim.hour - coluna.hora_inicio.hour)
-            elif(today.weekday()==4 and coluna.nome =='SEXTA'):
-                qtde = (coluna.hora_fim.hour - coluna.hora_inicio.hour)
-            elif(today.weekday()==5 and coluna.nome =='SABADO'):
-                qtde = (coluna.hora_fim.hour - coluna.hora_inicio.hour)
-            elif(today.weekday()==6 and coluna.nome =='Domingo'):
-                qtde = (coluna.hora_fim.hour - coluna.hora_inicio.hour)
+            if today.weekday() == 0 and coluna.nome == "SEGUNDA":
+                qtde = coluna.hora_fim.hour - coluna.hora_inicio.hour
+            elif today.weekday() == 1 and coluna.nome == "TERÇA":
+                qtde = coluna.hora_fim.hour - coluna.hora_inicio.hour
+            elif today.weekday() == 2 and coluna.nome == "QUARTA":
+                qtde = coluna.hora_fim.hour - coluna.hora_inicio.hour
+            elif today.weekday() == 3 and coluna.nome == "QUINTA":
+                qtde = coluna.hora_fim.hour - coluna.hora_inicio.hour
+            elif today.weekday() == 4 and coluna.nome == "SEXTA":
+                qtde = coluna.hora_fim.hour - coluna.hora_inicio.hour
+            elif today.weekday() == 5 and coluna.nome == "SABADO":
+                qtde = coluna.hora_fim.hour - coluna.hora_inicio.hour
+            elif today.weekday() == 6 and coluna.nome == "Domingo":
+                qtde = coluna.hora_fim.hour - coluna.hora_inicio.hour
         # print(qtde)
         for i in range(qtde):
             uma = timedelta(hours=qtde + i)
-            linha = linha + '<th>'+str(uma)+'</th>'
+            linha = linha + "<th>" + str(uma) + "</th>"
             for pro in proficionais:
                 # linha = linha + '<th></th>'
-                linha = linha + self.formatday(today.day,uma,pro,events)
+                linha = linha + self.formatday(today.day, uma, pro, events)
                 # print(linha)
-            linha = linha + '</tr>'
-            a(linha + '\n')
-            linha = ''
-        
-        a('\n')
-        a('</table>')
-        a('\n')
+            linha = linha + "</tr>"
+            a(linha + "\n")
+            linha = ""
+
+        a("\n")
+        a("</table>")
+        a("\n")
         # print(''.join(v))
-        return ''.join(v)
+        return "".join(v)
         return None
---
+++ salao/celery.py
@@ -0,0 +1,31 @@
+import os
+
+from celery import Celery
+from celery import task
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salao.settings")
+
+app = Celery("salao")
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    "teste": {
+        "task": "salao.salao.teste",
+        "schedule": 20,
+    },
+}
+
+
+@app.task
+def teste():
+    return "Corredor"
