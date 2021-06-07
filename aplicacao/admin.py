62d2d22ef8a1fd89fe5aaeba995f8e1491df6845
ricardosmbr <ricardosm@rede.ulbra.br>
2021-06-07 17:35:15 -0300

    add Export selected

--- aplicacao/admin.py
+++ aplicacao/admin.py
@@ -137,10 +137,30 @@ class AgendaServicoAdmin(admin.ModelAdmin):
         return super(AgendaServicoAdmin, self).changelist_view(request, extra_context)
 
 
-class CaixaAdmin(ImportExportModelAdmin):
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+class CaixaAdmin(ImportExportModelAdmin, ExportCsvMixin):
     list_display = ["data", "valor"]
     model = Caixa
     inlines = [PagamentoInline]
+    actions = ["export_as_csv"]
 
 
 admin.site.register(Clientes, ClientesAdmin)
